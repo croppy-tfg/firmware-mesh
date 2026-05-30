e9b1c7eaeb51a73f47fa53c165b43521ea2d5991
Jose <jose@miarma.net>
2026-05-31 00:15:47 +0200

    feat: Add basic I2C library for the OLED display

--- Drivers/SSD1306/Inc/ssd1306.h
+++ Drivers/SSD1306/Inc/ssd1306.h
@@ -1,8 +1,17 @@
+/**
+  * @file    ssd1306.h
+  * @author  JM
+  * @brief   Driver library for SSD1306 OLED display (128x32) using I2C for STM32F1.
+  * @date    2026
+  */
+
 #ifndef __SSD1306_H__
 #define __SSD1306_H__
 
 #include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_i2c.h"
 
+// 00111100 << 1 = 01111000 = addr [7:1] + r/w [0]
 #define SSD1306_I2C_ADDR        (0x3C << 1)
 
 #define SSD1306_COMMAND         0x00
@@ -11,8 +20,82 @@
 #define SSD1306_WIDTH           128
 #define SSD1306_HEIGHT          32
 
+typedef enum {
+    PIXEL_OFF = 0x00,
+    PIXEL_ON = 0x01
+} SSD1306_PixelState;
+
+/**
+  * @brief Initializes the display sending the appropiate I2C commands
+  * @param None
+  * @retval None
+  */
 void SSD1306_Init(void);
+
+/**
+  * @brief Sends a command to the display via I2C
+  * @param command: command to be sent
+  * @retval None
+  */
+void SSD1306_WriteCommand(uint8_t command);
+
+/**
+  * @brief Sends I2C data to the display
+  * @param data: the address of the first byte
+  * @param size: the number of bytes from data
+  * @retval None
+  */
+void SSD1306_WriteData(uint8_t *data, uint16_t size);
+
+/**
+  * @brief Writes the content of the frame buffer into de GDDRAM
+  * @param None
+  * @retval None
+  */
 void SSD1306_UpdateScreen(void);
-void SSD1306_DrawPixel(uint8_t x, uint8_t y);
+
+/**
+  * @brief Turns on/off the selected pixel.
+  * The byte index in the buffer is calculated using:
+  * * index = x + (y / 8) * 128
+  *
+  * Meaning: The page number (y / 8, as each page is 8 pixels high) 
+  * is multiplied by the page width (128 bytes) to skip all previous 
+  * pages. Then, the column offset (x) is added to find the exact byte.
+  *
+  * @param x: column [0, 127]
+  * @param y: row [0, 31]
+  * @param state: PIXEL_ON (0x01) or PIXEL_OFF (0x00)
+  * @retval None
+  */
+void SSD1306_DrawPixel(uint8_t x, uint8_t y, SSD1306_PixelState state);
+
+/**
+  * @brief Draws a monochrome bitmap on the screen
+  * @param x: starting column [0, 127]
+  * @param y: starting row [0, 31]
+  * @param bitmap: pointer to the image array
+  * @param width: image width in pixels
+  * @param height: image height in pixels
+  * @retval None
+  */
+void SSD1306_DrawBitmap(uint8_t x, uint8_t y, const uint8_t *bitmap, uint8_t width, uint8_t height);
+
+/**
+  * @brief Writes a letter from a font map into the display.
+  * @param index: the position of the letter in the map
+  * @param x: column [0, 127]
+  * @param y: row [0, 31]
+  * @retval None
+  * @version WIP
+  */
+void SSD1306_WriteLetter(uint8_t fontmap[][5], uint8_t index, uint8_t x, uint8_t y);
+
+/**
+  * @brief Clears the display
+  * @param None
+  * @retval None
+  */
+void SSD1306_Clear(void);
 
 #endif // __SSD1306_H__
